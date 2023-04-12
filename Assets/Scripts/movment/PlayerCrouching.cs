f7a607846906327067fa31c701706e6aedd38ca2
kyokomoo <112667234+kyokomoo@users.noreply.github.com>
2023-04-12 12:39:17 -0700

    player input from scratch complete only implemented in level 2 scene

--- Assets/Scripts/movment/PlayerCrouching.cs
+++ Assets/Scripts/movment/PlayerCrouching.cs
@@ -1,30 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-
-
 
 [RequireComponent(typeof(Player))]
 public class PlayerCrouching : MonoBehaviour
 {
     [SerializeField] float crouchHeight = 1f;
     [SerializeField] float crouchTransitionSpeed = 10f;
+    [SerializeField] float uncrouchTransitionSpeed = 20f;
     [SerializeField] float crouchSpeedMultiplier = .5f;
 
     Player player;
     PlayerInput playerInput;
     InputAction crouchAction;
 
-
     Vector3 initialCameraPosition;
-
     float currentHeight;
     float standingHeight;
 
     bool isCrouching => standingHeight - currentHeight > .1f;
-
 
     void Awake()
     {
@@ -33,8 +26,7 @@ public class PlayerCrouching : MonoBehaviour
         crouchAction = playerInput.actions["crouch"];
     }
 
-
-    void start()
+    void Start()
     {
         initialCameraPosition = player.cameraTransform.localPosition;
         standingHeight = currentHeight = player.Height;
@@ -43,59 +35,47 @@ public class PlayerCrouching : MonoBehaviour
     void OnEnable() => player.OnBeforeMove += OnBeforeMove;
     void OnDisable() => player.OnBeforeMove -= OnBeforeMove;
 
-
     void OnBeforeMove()
     {
-        var isTryingToCrouch = crouchAction.ReadValue<float>() > 0;
+        var isTryingToCrouch = crouchAction.ReadValue<float>() > 0.1f;
 
         var heightTarget = isTryingToCrouch ? crouchHeight : standingHeight;
 
-        if(isCrouching && !isTryingToCrouch)
+        if (isCrouching && !isTryingToCrouch)
         {
             var castOrigin = transform.position + new Vector3(0, currentHeight / 2, 0);
 
-            if( Physics.Raycast(castOrigin, Vector3.up, out RaycastHit hit, 0.02f))
+            if (Physics.Raycast(castOrigin, Vector3.up, out RaycastHit hit, 0.2f))
             {
                 var distanceToCeiling = hit.point.y - castOrigin.y;
 
                 heightTarget = Mathf.Max
                 (
-                    currentHeight + distanceToCeiling - 0.01f,
+                    currentHeight + distanceToCeiling - 0.1f,
                     crouchHeight
                 );
-
             }
-
-            
         }
 
-        if(!Mathf.Approximately(heightTarget, currentHeight))
-        {
-            var crouchDelta = Time.deltaTime * crouchTransitionSpeed;
+        var crouchDelta = Time.deltaTime * crouchTransitionSpeed;
 
-            currentHeight = Mathf.Lerp(currentHeight, heightTarget, crouchDelta);
+        currentHeight = Mathf.MoveTowards(currentHeight, heightTarget, crouchDelta);
 
-            var halfHeightDifference = new Vector3(0, (standingHeight - currentHeight) / 2, 0);
+        var halfHeightDifference = new Vector3(0, (standingHeight - currentHeight) / 2, 0);
 
-            var newCameraPosition = initialCameraPosition - halfHeightDifference;
+        var newCameraPosition = initialCameraPosition - halfHeightDifference;
 
-            player.cameraTransform.localPosition = newCameraPosition;
+        player.cameraTransform.localPosition = newCameraPosition;
 
-            player.Height = currentHeight;
-        }
-        
+        player.Height = currentHeight;
 
-        if(isCrouching)
+        if (isCrouching)
         {
             player.movementSpeedMultiplier *= crouchSpeedMultiplier;
         }
-
-
     }
+
 }
-
-
-
 
 
 
