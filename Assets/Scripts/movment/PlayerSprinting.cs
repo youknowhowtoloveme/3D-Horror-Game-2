97497f7473fdf8723f6c9ad890c43a4154bf9af2
kyokomoo <112667234+kyokomoo@users.noreply.github.com>
2023-04-17 09:21:46 -0700

    stamina settings are in code just have to make a ui for it

--- Assets/Scripts/movment/PlayerSprinting.cs
+++ Assets/Scripts/movment/PlayerSprinting.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using System;
 
 [RequireComponent(typeof(Player))]
 public class PlayerSprinting : MonoBehaviour
 {
-    [SerializeField] float SpeedMultiplier = 0.5f;
+    [SerializeField] float speedMultiplier = 0.5f;
+    [SerializeField] float staminaMax = 100f;
+    [SerializeField] float staminaDepletionRate = 10f;
+    [SerializeField] float staminaRegenRate = 20f;
+    [SerializeField] float sprintCost = 20f;
+
     Player player;
     PlayerInput playerInput;
     InputAction sprintAction;
+
+    float currentStamina;
 
     void Awake()
     {
         player = GetComponent<Player>();
         playerInput = GetComponent<PlayerInput>();
         sprintAction = playerInput.actions["sprint"];
+        currentStamina = staminaMax;
+        //UpdateStaminaUI();
     }
 
     void OnEnable() => player.OnBeforeMove += OnBeforeMove;
@@ -26,6 +36,31 @@ public class PlayerSprinting : MonoBehaviour
     {
         var sprintInput = sprintAction.ReadValue<float>();
 
-        player.movementSpeedMultiplier *= sprintInput > 0 ? SpeedMultiplier : 1f;
+        if (sprintInput > 0 && currentStamina >= sprintCost)
+        {
+            player.movementSpeedMultiplier *= speedMultiplier;
+            currentStamina -= sprintCost * Time.deltaTime * staminaDepletionRate;
+        }
+        else
+        {
+            player.movementSpeedMultiplier = 1f;
+            currentStamina += Time.deltaTime * staminaRegenRate;
+            //UpdateStaminaUI();
+        }
+
+        currentStamina = Mathf.Clamp(currentStamina, 0f, staminaMax);
+        //UpdateStaminaUI();
     }
+
+
+    /**void UpdateStaminaUI()
+    {
+        if (staminaBar != null)
+        {
+            staminaBar.fillAmount = currentStamina / staminaMax;
+        }
+    }
+    
+    */
+
 }
